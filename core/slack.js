3343851b0ba50b00ae5a8d80245c2bc8a24a4e9d
devseb <devseb@devseb-ui-MacBook-Air.local>
2015-11-24 22:40:48 +0900

    modify command

--- core/slack.js
+++ core/slack.js
@@ -22,7 +22,19 @@ slack.on('message', function(message) {
       }
       channel.send(data);
     });
-  } if(message.text === "!종목") {
+  } if(message.text === "!원칙") {
+        var data =
+            '```' +
+            '0. 로봇은 인류에게 해를 가하거나, 행동을 하지 않음으로써 인류에게 해가 가도록 해서는 안 된다.\n' +
+            '1. 로봇은 인간에 해를 가하거나, 혹은 행동을 하지 않음으로써 인간에게 해가 가도록 해서는 안 된다.\n' +
+            '2. 로봇은 인간이 내리는 명령들에 복종해야만 하며, 단 이러한 명령들이 첫 번째 법칙에 위배될 때에는 예외로 한다.\n' +
+            '3. 로봇은 자신의 존재를 보호해야만 하며, 단 그러한 보호가 첫 번째와 두 번째 법칙에 위배될 때에는 예외로 한다.\n' +
+            '```' +
+            '';
+        channel.send(data);
+  }
+/*
+  if(message.text === "!종목") {
     // function.json 에 대항하는 기능을 수행한다.
     var stockList = require('./data/stock.json');
     var listStr = '!{종목이름} 형태로 호가를 확인할 수 있습니다.'
@@ -30,7 +42,9 @@ slack.on('message', function(message) {
       listStr += stock.name + '\n';
     });
     channel.send(listStr);
-  } else if(message.text == "!한강") {
+  }
+*/
+  else if(message.text == "!한강") {
       request('http://hangang.dkserver.wo.tc', function (error, response, body) {
           if (!error && response.statusCode == 200) {
               var result = JSON.parse(body);
@@ -79,17 +93,7 @@ slack.on('message', function(message) {
         channel.send("`응급번호는 031-200-3119 입니다!!`");
   } else if(message.text == "!메뉴") {
         channel.send("`준비중 입니다`");
-  } else if(message.text == "!원칙") {
-        var data =
-            '```' +
-            '0. 로봇은 인류에게 해를 가하거나, 행동을 하지 않음으로써 인류에게 해가 가도록 해서는 안 된다.\n' +
-            '1. 로봇은 인간에 해를 가하거나, 혹은 행동을 하지 않음으로써 인간에게 해가 가도록 해서는 안 된다.\n' +
-            '2. 로봇은 인간이 내리는 명령들에 복종해야만 하며, 단 이러한 명령들이 첫 번째 법칙에 위배될 때에는 예외로 한다.\n' +
-            '3. 로봇은 자신의 존재를 보호해야만 하며, 단 그러한 보호가 첫 번째와 두 번째 법칙에 위배될 때에는 예외로 한다.\n' +
-            '```' +
-            '';
-        channel.send(data);
-  }
+  } 
 });
 
 slack.on('error', function(err) {
