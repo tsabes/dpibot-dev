6e492e6ed9c02373bef083ed2fa0f4516e804761
devseb <devseb@devseb-ui-MacBook-Air.local>
2015-11-28 02:19:22 +0900

    code refactoring

--- core/slack.js
+++ core/slack.js
@@ -1,28 +1,29 @@
 var fs = require('fs')
-  , path = require('path')
-  , _ = require('lodash')
-  , request = require('request')
-  , Slack = require('slack-client')
-  , debug = require('debug')('slack');
+    , path = require('path')
+    , _ = require('lodash')
+    , request = require('request')
+    , Slack = require('slack-client')
+    , debug = require('debug')('slack');
 
 var config = require('../core/config')
-  , slack = new Slack(config.slack.slackToken, config.slack.autoReconnect, config.slack.autoMark);
+    , slack = new Slack(config.slack.slackToken, config.slack.autoReconnect, config.slack.autoMark);
 
 slack.on('open', function() {
-  debug('open:', 'Connected to ' + slack.team.name + ' as ' + slack.self.name);
+      debug('open:', 'Connected to ' + slack.team.name + ' as ' + slack.self.name);
 });
 
 slack.on('message', function(message) {
-  var channel = slack.getChannelGroupOrDMByID(message.channel);
+    var channel = slack.getChannelGroupOrDMByID(message.channel);
 
-  if(message.text === "!") {
-    fs.readFile(path.join(__dirname, '/data/usage.info'), 'utf8', function(err, data) {
-      if(err) {
-        debug('error:', {err:err});
-      }
-      channel.send(data);
-    });
-  } if(message.text === "!원칙") {
+    if(message.text === "!") {
+        fs.readFile(path.join(__dirname, '/data/usage.info'), 'utf8', function(err, data) {
+        if(err) {
+            debug('error:', {err:err});
+        }
+        channel.send(data);
+        });
+    }
+    if(message.text === "!원칙") {
         var data =
             '```' +
             '0. 로봇은 인류에게 해를 가하거나, 행동을 하지 않음으로써 인류에게 해가 가도록 해서는 안 된다.\n' +
@@ -32,47 +33,49 @@ slack.on('message', function(message) {
             '```' +
             '';
         channel.send(data);
-  }
+    }
 /*
-  if(message.text === "!종목") {
-    // function.json 에 대항하는 기능을 수행한다.
-    var stockList = require('./data/stock.json');
-    var listStr = '!{종목이름} 형태로 호가를 확인할 수 있습니다.'
-    _.forEach(stockList, function(stock) {
-      listStr += stock.name + '\n';
-    });
-    channel.send(listStr);
-  }
+    if(message.text === "!종목") {
+        // function.json 에 대항하는 기능을 수행한다.
+        var stockList = require('./data/stock.json');
+        var listStr = '!{종목이름} 형태로 호가를 확인할 수 있습니다.'
+        _.forEach(stockList, function(stock) {
+        listStr += stock.name + '\n';
+        });
+        channel.send(listStr);
+    }
 */
-  else if(message.text == "!한강") {
-      request('http://hangang.dkserver.wo.tc', function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-              var result = JSON.parse(body);
-              channel.send("`많이 힘드시죠? 현재 한강물 온도는 " + result.temp + "°C 입니다.`");
-          } else {
-              channel.send("`죄송합니다. 현재 한강물 온도를 알아내지 못했습니다.`");
-          }
-      });
-  } else if(message.text == "!춤") {
-      var data = 
-          '⊂_ヽ\n' +
-          '　 ＼＼ Λ＿Λ\n' +
-          '　　 ＼( \'ㅅ\' ) 두둠칫\n' +
-          '　　　 >　⌒ヽ\n' +
-          '　　　/ 　 へ＼\n' +
-          '　　 /　　/　＼＼\n' +
-          '　　 ﾚ　ノ　　 ヽ_つ\n' +
-          '　　/　/ 두둠칫\n' +
-          '　 /　/|\n' +
-          '　(　(ヽ\n' +
-          '　|　|、＼\n' +
-          '　| 丿 ＼ ⌒)\n' +
-          '　| |　　) /\n' +
-          '(`ノ )　Lﾉ\n' +
-          '' +
-          '';
-          channel.send(data);
-  } else if(message.text == "!소개") {
+    else if(message.text == "!한강") {
+        request('http://hangang.dkserver.wo.tc', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var result = JSON.parse(body);
+                channel.send("`많이 힘드시죠? 현재 한강물 온도는 " + result.temp + "°C 입니다.`");
+            } else {
+                channel.send("`죄송합니다. 현재 한강물 온도를 알아내지 못했습니다.`");
+            }
+        });
+    }
+    else if(message.text == "!춤") {
+        var data = 
+            '⊂_ヽ\n' +
+            '　 ＼＼ Λ＿Λ\n' +
+            '　　 ＼( \'ㅅ\' ) 두둠칫\n' +
+            '　　　 >　⌒ヽ\n' +
+            '　　　/ 　 へ＼\n' +
+            '　　 /　　/　＼＼\n' +
+            '　　 ﾚ　ノ　　 ヽ_つ\n' +
+            '　　/　/ 두둠칫\n' +
+            '　 /　/|\n' +
+            '　(　(ヽ\n' +
+            '　|　|、＼\n' +
+            '　| 丿 ＼ ⌒)\n' +
+            '　| |　　) /\n' +
+            '(`ノ )　Lﾉ\n' +
+            '' +
+            '';
+        channel.send(data);
+    }
+    else if(message.text == "!소개") {
         channel.send("`안녕하세요. 여러분의 친구 DPI봇 입니다^^`");
         channel.send("`혼자보기: /dm @dpibot \"!명령\"`");
         var data =
@@ -87,21 +90,24 @@ slack.on('message', function(message) {
             '' +
             '';
         channel.send(data);
-  } else if(message.text == "!버전") {
+    }
+    else if(message.text == "!버전") {
         channel.send("`Version : v1.0.0`");
-  } else if(message.text == "!응급") {
+    }
+    else if(message.text == "!응급") {
         channel.send("`응급번호는 031-200-3119 입니다!!`");
-  } else if(message.text == "!메뉴") {
+    }
+    else if(message.text == "!메뉴") {
         channel.send("`준비중 입니다`");
-  } else if(message.text == "!버그") {
+    }
+    else if(message.text == "!버그") {
         channel.send("`Trouble Shooting : tsabs87@gmail.com`");
         channel.send("`mySingle ID : seba.lee`");
-  } 
+    } 
 });
 
 slack.on('error', function(err) {
-  debug('error:', {err:err});
+    debug('error:', {err:err});
 });
 
 slack.login();
-
