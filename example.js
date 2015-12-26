b2cf52e9e83944222284b55e61fe3c273aaf408d
Sebastian Lee <tsabes87@gmail.com>
2015-12-26 16:37:40 +0900

    initial parsing

--- example.js
+++ example.js
@@ -3,11 +3,11 @@ var cheerio = require('cheerio');
 
 
         var url = 'http://www.welstory.com/menu/Suwon_sec/foodcourt_R5.jsp?sDate=20151228'
-        request('http://www.welstory.com/menu/Suwon_sec/foodcourt_R5.jsp?sDate=20151228', function (error, response, html) {
+        request(url, function (error, response, html) {
             if (!error && response.statusCode == 200) {
                 $ = cheerio.load(html);
                 console.log("parsing");
-                $('#floor_all.box_floor.flr_titB1.flr_restaurant.restaurant_menu').each(function() {
+                $('.restaurant_menu').each(function() {
                     console.log("parsing");
                     console.log($(this).text());
                  });
