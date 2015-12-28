55a777c129cd3745cef3743c5342fd5fe5dbe060
Sebastian Lee <tsabes87@gmail.com>
2015-12-28 16:20:11 +0900

    web parsing success

--- example.js
+++ example.js
@@ -1,14 +1,23 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var Iconv = require('iconv').Iconv;
+var iconv = new Iconv('EUC-KR','UTF-8//TRANSLIT//IGNORE');
 
-
+        
         var url = 'http://www.welstory.com/menu/Suwon_sec/foodcourt_R5.jsp?sDate=20151228'
-        request(url, function (error, response, html) {
+        request({ uri : url, encoding : 'binary' }, function (error, response, body) {
             if (!error && response.statusCode == 200) {
-                $ = cheerio.load(html);
-                console.log("parsing");
-                $('.restaurant_menu').each(function() {
-                    console.log("parsing");
+                var strContents = new Buffer(body, 'binary');
+                strContents = iconv.convert(strContents).toString();
+                $ = cheerio.load(strContents);
+
+                $('#meal_2 .flr_restaurant').each(function() {
+                    console.log($(this).text());
+                 });
+                $('#meal_2 .restaurant_titB1').each(function() {
+                    console.log($(this).text());
+                 });
+                $('#meal_2 .restaurant_menu').each(function() {
                     console.log($(this).text());
                  });
 
@@ -17,5 +26,3 @@ var cheerio = require('cheerio');
                 console.log("error");
             }   
         });
-
-
